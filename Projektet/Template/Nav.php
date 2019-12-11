9ba55e83ecd3feb1b126322370e7d05a54faeb2b
LucasZGoransson <46808945+LucasZGoransson@users.noreply.github.com>
2019-12-11 10:49:10 +0100

    html inte php

--- Projektet/Template/Nav.php
+++
@@ -1,8 +0,0 @@
-<div class="divsidenav">
-	<nav role="navigation" class="sidenav">
-		<ul>
-			<li><a href="Hem.html">Hem</a></li>
-			<li><a href="LoggaIn.html" id="Active">Logga in</a></li>
-		</ul>
-	</nav>
-</div>
