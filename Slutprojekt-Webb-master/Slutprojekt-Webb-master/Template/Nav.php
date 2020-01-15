4d45d23666a1d2a648b90b67dbda64af4991fea0
LucasZGoransson <46808945+LucasZGoransson@users.noreply.github.com>
2020-01-15 11:03:42 +0100

    Fel i sparande

--- Slutprojekt-Webb-master/Slutprojekt-Webb-master/Template/Nav.php
+++
@@ -1,8 +0,0 @@
-<div class="divsidenav">
-	<nav role="navigation" class="sidenav">
-		<ul>
-			<li><a href="Index.php" id="Active">Hem</a></li>
-			<li><a href="LoggaIn.php">Logga in</a></li>
-		</ul>
-	</nav>
-</div>
